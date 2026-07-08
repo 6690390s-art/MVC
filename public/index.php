8f87d2d4034d8a40073b8805a111950025a3c76b
6690390s-art <6690390s@gmail.com>
2026-07-08 09:15:37 -0600

    Subir proyecto MVC completo

---
+++ public/index.php
@@ -0,0 +1,8 @@
+<?php
+
+require_once '../confing/database.php';
+
+require_once '../autoload.php';
+
+require_once '../routes/wed.php';
+
---
+++ wed.php
@@ -0,0 +1,16 @@
+<?php
+
+use lib\Route;
+use app\Controllers\HomeController;
+Route::get('/', [HomeController::class, 'index']
+);
+Route::get('/contact', function(){
+    return'hola desde la pagina de contacto';
+});
+Route::get('/about', function(){
+    return'hola desde la pagina acerca de';
+});
+Route::get('/courses/:slug', function($slug){
+    return'El curso es: ' . $slug;
+});
+Route::dispatch();
